c79ba411f82d86b561122392a207871657ddbbbe
p.bizimis <bizimisphilip@gmail.com>
2019-11-09 14:57:21 +0100

    added docs editor function

--- cmsapp/googleapi.py
+++ cmsapp/googleapi.py
@@ -14,14 +14,10 @@ def get_document(credentials, document_link):
    credentials = get_right_credentials(credentials)
    docs = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
    documentId = re.findall("/document/d/([a-zA-Z0-9-_]+)", document_link)[0]
-   #try:
    document = docs.documents().get(documentId=documentId).execute()
-   doc_content = document.get("body").get("content")
-   extracted_elements = extract_elements(doc_content)
-   print(extracted_elements)
+   content = get_content(document)
+   print(content)
    return document
-   #except:
-   #   return print("No permission")
 
 def get_user_info(credentials):
    credentials = get_right_credentials(credentials)
@@ -30,29 +26,42 @@ def get_user_info(credentials):
 
    return user_info
 
-def extract_elements(elements):
-   text = ''
-   for value in elements:
-      if 'paragraph' in value:
-         elements = value.get('paragraph').get('elements')
-         for elem in elements:
-               text += read_paragraph_element(elem)
-      elif 'table' in value:
-         # The text in table cells are in nested Structural Elements and tables may be
-         # nested.
-         table = value.get('table')
-         for row in table.get('tableRows'):
-               cells = row.get('tableCells')
-               for cell in cells:
-                  text += read_strucutural_elements(cell.get('content'))
-      elif 'tableOfContents' in value:
-         # The text in the TOC is also in a Structural Element.
-         toc = value.get('tableOfContents')
-         text += read_strucutural_elements(toc.get('content'))
-   return text
+def get_content(document):
+    result_string = ""
+    content = document.get("body").get("content")
+    for c in content:
+        if "paragraph" in c:
+            elements = c.get('paragraph').get('elements')
+            for element in elements:
+                text_run = element.get('textRun')
+                if not text_run:
+                    result_string += ""
+                else:
+                    result_string += text_run.get('content')
 
-def read_paragraph_element(element):
-    text_run = element.get('textRun')
-    if not text_run:
-        return ''
-    return text_run.get('content')
+    #split result_editor at \n
+    splitted_string_raw = result_string.split("\n")
+    splitted_string = list(filter(None, splitted_string_raw))
+    author = splitted_string[1]
+    title = document["title"]
+    tags = splitted_string[3].split(",")
+    content = {}
+
+    for counter, elem in enumerate(splitted_string):
+        if "Content" in elem:
+            para = 1
+            pic = 1
+            pic_indexes = []
+            for x in range(counter+1, len(splitted_string)):
+                if "Picture:" == splitted_string[x]:
+                    content["picture" + str(pic)] = splitted_string[x+1]
+                    pic_indexes.append(x+1)
+                    pic += 1
+                elif x not in pic_indexes:
+                    content["paragraph" + str(para)] = splitted_string[x]
+                    para += 1
+            break
+    
+    result_dict = {"author": author, "title": title, "tags": tags, "content": content}
+
+    return result_dict
