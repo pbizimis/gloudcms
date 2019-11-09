ab850800b690ea8f6648b6b238e7ecf646ef7907
p.bizimis <bizimisphilip@gmail.com>
2019-11-09 18:53:47 +0100

    added restapi microservice

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session
 from restapi.articles import articles
+import os
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
--- restapi/articles.py
+++ restapi/articles.py
@@ -4,11 +4,10 @@ from pymongo import MongoClient
 articles = Blueprint("artciles", __name__)
 
 DATABASE_URI = 'mongodb://localhost:27017/'
-
 client = MongoClient(DATABASE_URI)
 db = client.gloudcms
 
-@articles.route("/<admin_id>", methods=["GET"])
-def get_articles(admin_id):
-    
-    return jsonify(artcile)
+@articles.route("/<apiid>", methods=["GET"])
+def get_articles(apiid):
+    all_articles = list(db.article.find({"apiid": apiid}, {"_id": 0}))
+    return jsonify(all_articles)
